9c232f9cff0420e6dacfe761e31adee83f663187
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-05-01 16:28:47 +0530

    footer exercise

--- discover-coffee-stores/pages/_app.js
+++ discover-coffee-stores/pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div><Component {...pageProps} />
+     <footer>
+  <p>Author: Hege Refsnes</p>
+  <p><a href="mailto:hege@example.com">hege@example.com</a></p>
+</footer>
+  </div>
 }
