b7f0be719286eaa102d43017793d9659a1ebd490
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-05-01 17:21:56 +0530

    homepage started

--- discover-coffee-stores/pages/Hello.js
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/Hello.module.css';
-const Hello =()=>{
-    console.log(styles);
-    return <div className={styles.containeer}>Hello world!
-        This is codebase
-    </div>
-}
-
-export default Hello;
--- discover-coffee-stores/pages/_app.js
+++ discover-coffee-stores/pages/_app.js
@@ -2,9 +2,6 @@ import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return <div><Component {...pageProps} />
-     <footer>
-  <p>Author: Hege Refsnes</p>
-  <p><a href="mailto:hege@example.com">hege@example.com</a></p>
-</footer>
+  
   </div>
 }
