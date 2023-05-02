31ec4f5de5426507a2cab0cde9a2186a0a910a83
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-05-02 15:26:57 +0530

    adding font in Next js

--- discover-coffee-stores/pages/_document.js
+++ discover-coffee-stores/pages/_document.js
@@ -1,9 +1,33 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+
+
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link 
+        rel="preload" 
+        href="/fonts/IBMPlexsans-Bold.ttf"
+        as="font" 
+        crossOrigin="anonymous">
+
+        </link>
+        <link 
+        rel="preload" 
+        href="/fonts/IBMPlexsans-Regular.ttf"
+        as="font" 
+        crossOrigin="anonymous">
+          
+        </link>
+        <link 
+        rel="preload" 
+        href="/fonts/IBMPlexsans-SemiBold.ttf"
+        as="font" 
+        crossOrigin="anonymous">
+          
+        </link>
+      </Head>
       <body>
         <Main />
         <NextScript />
