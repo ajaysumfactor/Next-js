43f441856adafb1ba1f277484cca076deb952d16
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-05-04 13:02:35 +0530

    grid layout

--- discover-coffee-stores/components/card.js
+++ discover-coffee-stores/components/card.js
@@ -4,7 +4,8 @@ import styles from './card.module.css';
 import cls from 'classnames';
 const card = (props) => {
     return <div>
-        <Link href={props.href} className={styles.cardLink}>
+        <Link href={props.href} >
+            <div className={styles.cardLink}>
             <div className={cls("glass",styles.container)}>
                 <div className={styles.cardHeaderWrapper}>
                     <h2 className={styles.cardHeader}>{props.name}</h2>
@@ -12,6 +13,7 @@ const card = (props) => {
                 <div className={styles.cardImageWrapper}>
                     <Image className={styles.cardImage} src={props.imgUrl} width={260} height={160} />
                 </div>
+            </div>
             </div>
         </Link>
     </div>
