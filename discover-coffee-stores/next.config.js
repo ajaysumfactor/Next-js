cbbdd9e259f9591c44f2411ac1313039be3a2f19
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-05-04 14:46:55 +0530

    next.config.js implemented

--- discover-coffee-stores/next.config.js
+++ discover-coffee-stores/next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+ 
+module.exports = {
+  images: {
+    domains: ["images.unsplash.com"],
+  },
+};
 
-module.exports = nextConfig
+ 
