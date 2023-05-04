a36f044b72943f7b2ef5e51cd3147fe48eaedd4a
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-05-05 00:50:21 +0530

    fixed Card css

--- discover-coffee-stores/components/card.js
+++ discover-coffee-stores/components/card.js
@@ -3,9 +3,9 @@ import Image from 'next/image';
 import styles from './card.module.css';
 import cls from 'classnames';
 const card = (props) => {
-    return <div>
-        <Link href={props.href} >
-            <div className={styles.cardLink}>
+    return <>
+        <Link href={props.href} className={styles.cardLink} >
+            
             <div className={cls("glass",styles.container)}>
                 <div className={styles.cardHeaderWrapper}>
                     <h2 className={styles.cardHeader}>{props.name}</h2>
@@ -14,8 +14,7 @@ const card = (props) => {
                     <Image className={styles.cardImage} src={props.imgUrl} width={260} height={160} />
                 </div>
             </div>
-            </div>
         </Link>
-    </div>
+    </>
 }
 export default card;
