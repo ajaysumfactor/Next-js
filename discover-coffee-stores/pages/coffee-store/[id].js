5e485823aeb4b8e810638a1bc6b77313085a5e23
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-05-05 11:53:04 +0530

    implement get static path and get static paths

--- discover-coffee-stores/pages/coffee-store/[id].js
+++ discover-coffee-stores/pages/coffee-store/[id].js
@@ -1,16 +1,46 @@
 import Link from 'next/link';
-import { useRouter } from 'next/router'
-const CoffeeStore=()=>{
-    const route=useRouter();
+import { useRouter } from 'next/router';
+import coffeeStoreData from '../../data/coffee-stores.json';
+
+export function getStaticProps(staticProps) {
+    const params = staticProps.params;
+    console.log(params);
+    return {
+        props: {
+            CoffeeStore: coffeeStoreData.find(CoffeeStore => {
+                return CoffeeStore.id === 0;
+            }),
+        },
+    };
+}
+
+
+export function getStaticPaths() {
+    return {
+        paths: [
+            { params: { id: '1' } },
+            { params: { id: '2' } }
+        ]
+    };
+}
+
+
+
+
+
+
+
+const CoffeeStore = () => {
+    const route = useRouter();
     console.log(route);
     // return <div>Coffee store page</div>
     return <div>
-        <p>Post : {route.query.id}</p> 
+        <p>Post : {route.query.id}</p>
         <Link href="/">
             Back to the home
         </Link>
         <Link href="/coffee-store/dynamic">
-            Go to s page dynamic 
+            Go to s page dynamic
         </Link>
     </div>
 }
