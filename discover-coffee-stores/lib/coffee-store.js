cdde6241523e9349e9871dd576c0af32b3b2c902
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-05-08 16:50:57 +0530

    7/105 Display coffee store image in the card

--- discover-coffee-stores/lib/coffee-store.js
+++ discover-coffee-stores/lib/coffee-store.js
@@ -1,46 +1,53 @@
 //unsplash--
 import { createApi } from 'unsplash-js';
- 
+
 const unsplash = createApi({
   accessKey: process.env.UNSPLASH_ACCESS_KEY,
-  
+
 });
 
 
 
 
 
-const getUrlForCoffeeStores=(latLong,query,limit)=>{
-return `https://api.foursquare.com/v3/places/search?query=${query}&ll=${latLong}&limit=${limit}`
+const getUrlForCoffeeStores = (latLong, query, limit) => {
+  return `https://api.foursquare.com/v3/places/search?query=${query}&ll=${latLong}&limit=${limit}`
 };
 
-export const fetchCoffeeStores=async()=>{
-  unsplash.search.getPhotos({
-    query: 'cat',
+
+const getListOfCoffeeStorePhoto = async () => {
+  const photos = await unsplash.search.getPhotos({
+    query: 'coffie shop',
     page: 1,
-    perPage: 10,
-    color: 'green',
-    orientation: 'portrait',
+    perPage: 1,
   });
+  const arrayResults = photos.response.results.map((res) => res.urls["small"]); //showing the particular url here ;
+  return arrayResults;
+}
 
 
+export const fetchCoffeeStores = async () => {
+  const photo=await getListOfCoffeeStorePhoto();
 
-
-
-const options = {
+  const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
       Authorization: process.env.FOURSQUARE_API_KEY,
     }
   };
-  
+
   const response = await fetch(getUrlForCoffeeStores(
     "19.084124283768357,72.88391149751224",
     "coffee",
     6
   ), options)
   const data = await response.json();
-  return data.results;
+  return data.results.map((result)=>{
+    return{
+    ...result,
+    imgUrl: photo[0],
+    };
+  });
 
 };
