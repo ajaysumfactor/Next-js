86edc0752e6f021a7a27bdb1926eca488614a086
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-05-15 18:44:48 +0530

    10/143 basic coffee store api serverless functions + install Postman gone through (POST AND GET Request through Postman

--- discover-coffee-stores/pages/api/createCoffeeStore.js
+++ discover-coffee-stores/pages/api/createCoffeeStore.js
@@ -1,3 +1,19 @@
+
+
 const Airtable = require('airtable');
 const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_KEY);
 const table=base('coffee-stores');
+console.log({table});
+
+const createCoffeStore = (req,res) => {//by default it is get request 
+    console.log(req);//method : GET By default 
+    if(req.method=='POST'){
+        res.json({message: "hii! buddy"});
+
+    }
+    else{
+        res.json({message: "Method is GET"})
+    }
+};
+
+export default createCoffeStore;
