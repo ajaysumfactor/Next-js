3c73586e8be7e021dcfddffd05208618385eaf97
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-05-16 11:26:00 +0530

    10/145 find coffee store API list a record using select implement done +Airtable

--- discover-coffee-stores/pages/api/createCoffeeStore.js
+++ discover-coffee-stores/pages/api/createCoffeeStore.js
@@ -2,14 +2,20 @@
 
 const Airtable = require('airtable');
 const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_KEY);
-const table=base('coffee-stores');
+const table=base('coffee-stores');//store reference to a table in airtable websites 
 console.log({table});
 
-const createCoffeStore = (req,res) => {//by default it is get request 
+const createCoffeStore = async(req,res) => {//by default it is get request 
     console.log(req);//method : GET By default 
     if(req.method=='POST'){
-        res.json({message: "hii! buddy"});
-
+        const findCoffeeStoreRecords=await table.select({maxRecords: 1,}).firstPage();
+        console.log(findCoffeeStoreRecords);//data is in [{preoperty: value,property: value}]
+        if(findCoffeeStoreRecords.length!==0){
+            res.json(findCoffeeStoreRecords);
+        }
+        else{
+            res.json({message: "create a record"});
+        }
     }
     else{
         res.json({message: "Method is GET"})
