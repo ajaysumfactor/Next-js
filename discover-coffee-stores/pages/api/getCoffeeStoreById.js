f58e55338220776735861a169e0dbe3060f82909
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-05-17 11:14:41 +0530

    11/158 Coffee store by Id API Validation get data from airtable database and verify in postman

--- discover-coffee-stores/pages/api/getCoffeeStoreById.js
+++ discover-coffee-stores/pages/api/getCoffeeStoreById.js
@@ -1,18 +1,28 @@
-const getCoffeeStoreById=(req,res)=>{
-  const {id}=req.query;
-  try{
-    if(id){
-        res.json({message:`id is created ${id}`});
+import { table, getMinifiedRecords } from "../../lib/airTable";
 
+const getCoffeeStoreById = async (req, res) => {
+    const { id } = req.query;
+    try {
+        if (id) {
+            const findCoffeeStoreRecords = await table.select({ filterByFormula: `id="${id}"` }).firstPage();
+            // console.log(findCoffeeStoreRecords);//data is in [{preoperty: value,property: value}]
+            if (findCoffeeStoreRecords.length !== 0) {
+                const records = getMinifiedRecords(findCoffeeStoreRecords);
+                res.json(records);
+            }
+            else {
+                res.json({ message: "Id could not be found " });
+            }
+
+        }
+        else {
+            res.status(400);
+            res.json({ message: "Id is missing" });
+        }
     }
-    else{
-        res.status(400);
-        res.json({message:"Id is missing"});
+    catch (err) {
+        res.status(500);
+        res.json({ message: "Something went wrong ", err });
     }
-  }
-  catch(err){
-    res.status(500);
-    res.json({message: "Something went wrong ",err});
-  }
 }
 export default getCoffeeStoreById;
