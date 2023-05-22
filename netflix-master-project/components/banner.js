1a5124ed71e61bbe29bf7e50533b5cd97368bf85
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-05-22 16:32:19 +0530

    13/203 add the play icon

--- netflix-master-project/components/banner.js
+++ netflix-master-project/components/banner.js
@@ -1,4 +1,6 @@
+
 import styles from './banner.module.css'
+import Image from 'next/image';
 const Banner = (props) => {
     const { title, subTitle, imgUrl } = props;
     const handleOnPlay = () => {
@@ -13,22 +15,29 @@ const Banner = (props) => {
                         <p className={styles.firstLetter}>N</p>
                         <p className={styles.series}>S E R I E S</p>
                     </div>
-                    
-                        <h3 className={styles.title}>{title}</h3>
-                        <h3 className={styles.subTitle}>{subTitle}</h3>
+
+                    <h3 className={styles.title}>{title}</h3>
+                    <h3 className={styles.subTitle}>{subTitle}</h3>
 
 
 
-                        <div className={styles.playBtnWrapper}>
-                            <button className={styles.btnWithIcon} onClick={handleOnPlay}> <span className={styles.playText}>Play</span></button>
-                        </div>
-                        </div>
-                </div>
-                <div className={styles.bannerImg} style={{ backgroundImage: `url(${imgUrl}`, }}>
-
+                    <div className={styles.playBtnWrapper}>
+                        <button className={styles.btnWithIcon} onClick={handleOnPlay}> 
+                        <Image
+                            src="static/icons/play_arrow.svg"
+                            alt="Play icon"
+                            width="32"
+                            height="32"
+                        />
+                        <span className={styles.playText}>Play</span></button>
+                    </div>
                 </div>
             </div>
-            
-            );
+            <div className={styles.bannerImg} style={{ backgroundImage: `url(${imgUrl}`, }}>
+
+            </div>
+        </div>
+
+    );
 };
-            export default Banner;
+export default Banner;
